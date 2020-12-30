2ec5894d4a04492c759ca0c83d5184f414f98ead
Wesley <wes.beck21@outlook.com>
2020-12-30 12:13:48 -0800

    commit tree 1c967586f84f049022690cbfa6f1aa6033736ed6

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -6,4 +6,43 @@ class ApplicationController < Sinatra::Base
 
   # code actions here!
 
+  get "/recipes/new" do #loads new recipe form
+    erb :new
+  end
+
+  get "/recipes" do #loads index page
+    @recipes = Recipe.all
+    erb :index
+  end
+
+  get "/recipes/:id" do #loads show page
+    @recipe = Recipe.find_by_id(params[:id])
+    erb :show
+  end
+
+  get "/recipes/:id/edit" do #loads edit form
+    @recipe = Recipe.find_by_id(params[:id])
+    erb :edit
+  end
+
+  patch '/recipes/:id' do #updates recipe info and redirects to show page
+    @recipe = Recipe.find_by_id(params[:id])
+    @recipe.name = params[:name]
+    @recipe.ingredients = params[:ingredients]
+    @recipe.cook_time = params[:cook_time]
+    @recipe.save
+    redirect to "/recipes/#{@recipe.id}"
+  end
+
+  post "/recipes" do #creates a recipe
+    @recipe = Recipe.create(params)
+    redirect to "recipes/#{@recipe.id}"
+  end
+
+  delete "/recipes/:id" do
+    @recipe = Recipe.find_by_id(params[:id])
+    @recipe.delete
+    redirect to "/recipes"
+  end
+
 end
